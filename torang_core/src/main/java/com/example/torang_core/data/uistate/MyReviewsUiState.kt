da96e5e1c0d5174d542f4fca18062bc1e40fadb3
Sarang Yang <>
2022-04-26 18:56:23 +0900

    update

--- torang_core/src/main/java/com/example/torang_core/data/uistate/MyReviewsUiState.kt
+++ torang_core/src/main/java/com/example/torang_core/data/uistate/MyReviewsUiState.kt
@@ -3,9 +3,10 @@ package com.example.torang_core.data.uistate
 import com.example.torang_core.data.data.MyReview
 
 data class MyReviewUiState(
-    val isLogin: Boolean = false,
+    val isLogin: Boolean? = null,
     val isLoading: Boolean = false,
     val list: List<MyReview>? = null,
 )
 
 val MyReviewUiState.isEmpty: Boolean get() = (list != null && list.isEmpty())
+val MyReviewUiState.isScreenLoading: Boolean get() = (isLogin == null && list == null)
