36234baae6dc31891d31c329a18f48f3967c4975
Sarang Yang <>
2022-04-26 19:26:12 +0900

    update

--- torang_core/src/main/java/com/example/torang_core/data/uistate/MyReviewsUiState.kt
+++ torang_core/src/main/java/com/example/torang_core/data/uistate/MyReviewsUiState.kt
@@ -6,7 +6,10 @@ data class MyReviewUiState(
     val isLogin: Boolean? = null,
     val isLoading: Boolean = false,
     val list: List<MyReview>? = null,
+    val isfirstLoading: Boolean = false
 )
 
 val MyReviewUiState.isEmpty: Boolean get() = (list != null && list.isEmpty())
 val MyReviewUiState.isScreenLoading: Boolean get() = (isLogin == null && list == null)
+val MyReviewUiState.firstLoadingDate: Boolean get() = (!isfirstLoading && isLogin == true && list == null)
+//첫진입 시 로그인 상태이고 리스트가 비어있을 때 데이터 가져오기 필요
