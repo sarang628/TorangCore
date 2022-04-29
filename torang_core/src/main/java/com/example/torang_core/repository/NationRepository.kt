f7c65f145659108b119fe17eb65d48b31bc65ac6
Sarang Yang <>
2022-04-29 17:39:44 +0900

    update

--- torang_core/src/main/java/com/example/torang_core/repository/NationRepository.kt
+++ torang_core/src/main/java/com/example/torang_core/repository/NationRepository.kt
@@ -2,9 +2,11 @@ package com.example.torang_core.repository
 
 import com.example.torang_core.data.NationItem
 import com.example.torang_core.data.model.Restaurant
+import kotlinx.coroutines.flow.Flow
 
 interface NationRepository : MapSharedRepository {
     suspend fun getNationItems(): List<NationItem>
-
-    suspend fun findRestaurant() : List<Restaurant>
+    suspend fun findRestaurant(): List<Restaurant>
+    fun getSelectNationItem(): Flow<NationItem>
+    suspend fun selectNationItem(nationItem: NationItem)
 }
