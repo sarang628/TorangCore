8f050084eeaff532bc3568a1ab6963934a5d61d8
sryang <sarang628@naver.com>
2022-06-03 08:23:48 +0900

    arrange comment

--- torang_core/src/main/java/com/example/torang_core/repository/FindRepository.kt
+++ torang_core/src/main/java/com/example/torang_core/repository/FindRepository.kt
@@ -6,55 +6,34 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 interface FindRepository {
-    /** 검색 된 맛집 */
-    fun getSearchedRestaurant(): Flow<List<Restaurant>>
-
     /** 맛집 검색 */
-    suspend fun searchRestaurant(
-        distances: Distances? = null,
-        restaurantType: ArrayList<RestaurantType>? = null,
-        prices: Prices? = null,
-        ratings: ArrayList<Ratings>? = null,
-        latitude: Double = 0.0,
-        longitude: Double = 0.0,
-        northEastLatitude: Double = 0.0,
-        northEastLongitude: Double = 0.0,
-        southWestLatitude: Double = 0.0,
-        southWestLongitude: Double = 0.0,
-        searchType: SearchType
-    )
-
-    // 최초위치요청 상태 가져오기
-    fun getIsFirstRequestLocation(): StateFlow<Boolean>
-
-    // 상태를 가져오고 뷰에서 위치 요청을 했다면 요청했다고 저장소에 알려주기
+    suspend fun searchRestaurant(distances: Distances? = null, restaurantType: ArrayList<RestaurantType>? = null, prices: Prices? = null, ratings: ArrayList<Ratings>? = null, latitude: Double = 0.0, longitude: Double = 0.0, northEastLatitude: Double = 0.0, northEastLongitude: Double = 0.0, southWestLatitude: Double = 0.0, southWestLongitude: Double = 0.0, searchType: SearchType)
+    /** 상태를 가져오고 뷰에서 위치 요청을 했다면 요청했다고 저장소에 알려주기 */
     suspend fun notifyRequestLocation(): RequestLocationResult
-
-    // 현재 위치를 요청중인지
-    fun isRequestingLocation(): StateFlow<Boolean>
-
-    // 위치를 받아왔을때 알려줘야함
+    /** 위치를 받아왔을때 알려줘야함 */
     suspend fun notifyReceiveLocation()
-
-    // 이 지역 검색 요청
+    /** 이 지역 검색 요청 */
     suspend fun searchBoundRestaurant()
-
-    /** 검색 요청 트리거 */
-    fun getSearchBoundRestaurnatTrigger(): StateFlow<Boolean>
-
     /** 현재 포커스된 레스토랑의 위치 값 */
     suspend fun setCurrentPosition(position: Int)
-    fun getCurrentPosition(): StateFlow<Int>
-
-    suspend fun isFirstRequestLocationPermission(): StateFlow<Boolean>
-
-    /**
-     *  위치원한 요청에 대한 사용자 응답
-     */
+    /** 위치원한 요청에 대한 사용자 응답 */
     suspend fun requestLocationPermission(b: Boolean)
-
-    fun hasGrantPermission(): MutableStateFlow<Int>
+    /** */
     suspend fun permissionGranated()
+
+    /** 검색 된 맛집 */
+    fun getSearchedRestaurant(): Flow<List<Restaurant>>
+    /** 최초위치요청 상태 가져오기 */
+    fun getIsFirstRequestLocation(): StateFlow<Boolean>
+    /** 현재 위치를 요청중인지 */
+    fun isRequestingLocation(): StateFlow<Boolean>
+    /** 검색 요청 트리거 */
+    fun getSearchBoundRestaurnatTrigger(): StateFlow<Boolean>
+    /** 현재 포커스된 맛집 위치 */
+    fun getCurrentPosition(): StateFlow<Int>
+    /** 권한 소유 여부 */
+    fun hasGrantPermission(): MutableStateFlow<Int>
+
 }
 
 enum class RequestLocationResult {
