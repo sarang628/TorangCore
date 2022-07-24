c3810bea8a33e1f2772b0ea5938ec92dfec59983
Sarang Yang <sarang628@naver.com>
2022-07-24 11:52:38 +0900

    update

--- torang_core/src/main/java/com/example/torang_core/data/remote/RemoteFeed.kt
+++ torang_core/src/main/java/com/example/torang_core/data/remote/RemoteFeed.kt
@@ -1,5 +1,10 @@
 package com.example.torang_core.data.remote
 
 data class RemoteFeed(
-    val reviewId : Int
+    val name: String,
+    val reviewId: Int,
+    val restaurantName: String,
+    val rating: Float,
+    val profilePictureUrl: String,
+    val reviewImages: ArrayList<String>
 )
